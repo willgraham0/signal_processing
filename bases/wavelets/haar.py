4a86b67e409659318e029476f5f2890ec9710218
Will Graham <will.graham0@gmail.com>
2018-06-13 10:55:22 +0100

    Added haar_wavelet function

--- bases/wavelets/haar.py
+++ bases/wavelets/haar.py
@@ -13,9 +13,19 @@ def haar_matrix(m):
     """
     if not np.log2(n).is_integer():
         raise ValueError("The value of log2(m) must be a whole number.")
-    vT = np.ones
+    number_of_wavelets = m - 1
 
-def square(k, m):
+
+
+
+def haar_wavelet(k, m):
+    """Return a Haar wavelet of length k spanning the signal length m,
+    where k <= m. The values of log2(k) and log2(m) must be integers. 
+    E.g. square(2, 8) -> np.array([1, -1,  0,  0,  0,  0,  0,  0])
+    E.g. square(4, 8) -> np.array([1,  1, -1, -1,  0,  0,  0,  0])
+    """
+    if not np.log2(k).is_integer() and not np.log(m).is_integer():
+        raise ValueError("The values of log2(k) and log2(m) must be whole numbers.")
     x = np.sin(np.linspace(0, 2*np.pi, k))
     s = np.piecewise(x, [x < 0, x >= 0], [-1, 1])
     return np.append(s, np.zeros(m - k))
