b1d9e341d3d7ac8c510cae427a388701495b2fec
Will Graham <will.graham0@gmail.com>
2018-10-29 17:02:13 +0000

    Reduced verbosity of is_implemented function in transforms.py

--- sp/bases/wavelets/transforms.py
+++ sp/bases/wavelets/transforms.py
@@ -11,10 +11,7 @@ from .families import Wavelets, Haar
 
 def is_implemented(family):
     """Return True if the wavelet family is implemented. Else False."""
-    if family in Wavelets.families.keys():
-        return True
-    else:
-        return False
+    return family in Wavelets.families.keys()
 
 
 def get_family(family):
